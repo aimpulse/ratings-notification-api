57fb9762eaecd4b21c97006c08607d679679d0ca
Arne Schuldt <arne.schuldt@aimpulse.com>
2016-01-18 15:47:20 +0100

    Code formatting

--- webhook.php
+++ webhook.php
@@ -1,38 +1,37 @@
 <?php
+define("ERROR_FILE", dirname(__FILE__) . "/error.txt");
+define("LOG_FILE", dirname(__FILE__) . "/ratings.txt");
 
-define ( "ERROR_FILE", dirname ( __FILE__ ) . "/error.txt" );
-define ( "LOG_FILE", dirname ( __FILE__ ) . "/ratings.txt" );
-
-$body = file_get_contents ( "php://input" );
-$notification = json_decode ( $body, true );
+$body = file_get_contents("php://input");
+$notification = json_decode($body, true);
 
 if ($notification == NULL) {
-	http_response_code ( 500 );
-	file_put_contents ( ERROR_FILE, $body . "\n", FILE_APPEND | LOCK_EX );
-	exit ();
+	http_response_code(500);
+	file_put_contents(ERROR_FILE, $body . "\n", FILE_APPEND | LOCK_EX);
+	exit();
 }
 
-foreach ( $notification ["messages"] as $message ) {
+foreach ($notification ["messages"] as $message) {
 	if ($message ["type"] != "menu_rating") {
 		continue;
 	}
 	$poi = $message ["poi"];
-	writeLog ( $poi ["country"] . "\t" );
-	writeLog ( $poi ["location"] . "\t" );
-	writeLog ( $poi ["poi"] . "\t" );
+	writeLog($poi ["country"] . "\t");
+	writeLog($poi ["location"] . "\t");
+	writeLog($poi ["poi"] . "\t");
 	$rating = $message ["rating"];
-	writeLog ( $rating ["id"] . "\t" );
-	writeLog ( $rating ["timestamp"] . "\t" );
-	writeLog ( $rating ["stars"] . "\t" );
+	writeLog($rating ["id"] . "\t");
+	writeLog($rating ["timestamp"] . "\t");
+	writeLog($rating ["stars"] . "\t");
 	$meal = $rating ["meal"];
-	writeLog ( $meal ["day"] . "\t" );
+	writeLog($meal ["day"] . "\t");
 	$category = $meal ["category"];
-	writeLog ( $category ["position"] . "\t" );
-	writeLog ( $meal ["position"] . "\n" );
+	writeLog($category ["position"] . "\t");
+	writeLog($meal ["position"] . "\n");
 }
 
 function writeLog($content) {
-	file_put_contents ( LOG_FILE, $content, FILE_APPEND | LOCK_EX );
+	file_put_contents(LOG_FILE, $content, FILE_APPEND | LOCK_EX);
 }
 
 ?>
